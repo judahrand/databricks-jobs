e42266ac22c89120a1b2cd034b0688c424f54a85
Judah Rand <17158624+judahrand@users.noreply.github.com>
2023-03-16 14:20:18 +0000

    Only store the diff

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+wget https://learn.microsoft.com/en-us/azure/databricks/_extras/api-refs/jobs-2.1-azure.yaml \
+    -O schema/jobs-2.1-azure.yaml
+
+patch schema/jobs-2.1-azure.yaml schema/jobs-2.1-azure.diff
+
 openapi-generator generate \
     -i schema/jobs-2.1-azure.yaml \
     -g python-nextgen \
